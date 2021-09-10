4331672a3aa32405a5534ff6279816fc83bf4fa7
Alexander Russanow <alexander.russanow@gmail.com>
2021-09-10 14:38:19 +0200

    refactoring

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -1,28 +1,29 @@
-import { Badge, Calendar } from 'antd'
-import { Moment } from 'moment'
-import React from 'react'
-import { Event } from '../models/event'
+import { Calendar } from "antd";
+import { Moment } from "moment";
+import React from "react";
+import { Event } from "../models/event";
+import { dateTransorm } from "../utilits/dateTransform";
 
 type PropsType = {
-   events: Event[]
- }
+  events: Event[];
+};
 
-const CalendarComponent: React.FC<PropsType> = ({events}) => {
-   // function dateCellRender(value: Moment) {
-   //    const listData = getListData(value);
-   //    return (
-   //      <ul className="events">
-   //        {listData.map(item => (
-   //          <li key={item.content}>
-   //            <Badge status={item.type} text={item.content} />
-   //          </li>
-   //        ))}
-   //      </ul>
-   //    );
-   //  }dateCellRender={dateCellRender}
-   return (
-      <Calendar />
-   )
-}
+const CalendarComponent: React.FC<PropsType> = ({ events }) => {
+  function dateCellRender(value: Moment) {
+    const listData = dateTransorm(value.toDate());
+    const currentDate = events.filter((e) => e.date === listData);
 
-export default CalendarComponent
+    return (
+      <ul>
+        {currentDate.map((item, index) => (
+          <li key={index}>
+            <div>{item.description}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return <Calendar dateCellRender={dateCellRender} />;
+};
+
+export default CalendarComponent;
